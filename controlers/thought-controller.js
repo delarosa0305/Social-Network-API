08b8a0d3c3ac0ea143a44ec574f79c066e870062
delarosa0305 <delarosa2381i@gmail.com>
2022-08-21 14:53:50 -0400

    made frame for thought and user controllers

---
+++ controlers/thought-controller.js
@@ -0,0 +1,26 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    getAllThought(req, res) {
+    },
+
+    getThoughtById( { params }, res) {
+    },
+
+    createThought({ body }, res) {
+    },
+
+    updateThoughtById({ params, body }, res) {
+    },
+
+    deleteThoughtById({ params }, res) {
+    },
+
+    createReaction({ body }, res) {
+    },
+
+    deleteReactionById({ params }, res) {
+    }
+}
+
+module.exports = thoughtController
---
+++ controlers/user-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUser(req, res) {
+    },
+
+    getUserById( { params }, res) {
+    },
+
+    createUser({ body }, res) {
+    },
+
+    updateUserById({ params, body }, res) {
+    },
+
+    deleteUserById({ params }, res) {
+    }
+}
+
+module.exports = userController
